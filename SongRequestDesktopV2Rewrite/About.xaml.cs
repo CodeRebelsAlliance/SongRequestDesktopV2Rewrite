f4cd45f82656d0bf08f2420c871cb9a4913567ed
le MineGaming <leMineGamingYT@gmx.de>
2026-04-22 16:56:09 +0200

    version bump

--- SongRequestDesktopV2Rewrite/About.xaml.cs
+++ SongRequestDesktopV2Rewrite/About.xaml.cs
@@ -8,7 +8,7 @@ namespace SongRequestDesktopV2Rewrite
 {
     public partial class About : Window
     {
-        public static string version = "2.8";
+        public static string version = "2.9";
         
         public About()
         {
